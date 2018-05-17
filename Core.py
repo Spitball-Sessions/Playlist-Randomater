942e38ae9a414d2f942eec96fe764f8c7266af15
Koholos <Koholos@yahoo.com>
2018-05-16 20:55:56 -0400

    Prints each song to a different line in the .csv file

--- Core.py
+++ Core.py
@@ -1,34 +1,48 @@
 
-def open_list():
-    pass
+def create_csv(list):
+    with open("Song List.csv","w",encoding='utf-8') as csvfile:
+        songs = csv.writer(csvfile, delimiter = "\n")
+        songs.writerows([song_list])
+    print("Files added...")
+
 
 def find_files():
     is_last_directory = False
     still_adding = "yes"
+    song_list = []
+    music_dir_list = []
 
     while is_last_directory == False:
         music_dir = input("Where is your music directory located?\n")
         os.chdir(music_dir)
         check_dir = os.getcwd()
         print("Currently looking inside {}".format(check_dir))
-        for song_name in glob.iglob(".\**\*.mp3", recursive=True):
-            print(song_name)
+        for song_name in glob.iglob(check_dir + "\**\*.mp3", recursive=True):
+            song_list.append(song_name)
+
         while still_adding != "no" or "n":
-            still_adding = input("Do you have any other directories you'd like to add? ")
+            still_adding = input("Do you have any other directories you'd like to add? ").lower().strip()
             if still_adding == "yes" or still_adding == "y":
+                music_dir_list.append(music_dir)
                 break
             elif still_adding == "no" or still_adding == "n":
-                is_last_directory = True
-                break
+                final_song_list = [i + "\n" for i in song_list]
+                if music_dir_list == []:
+                    return final_song_list, music_dir
+                else:
+                    music_dir_list.append(music_dir)
+                    return final_song_list,music_dir_list
             else:
                 print("Please try that again. ")
                 continue
 
 
+def get_abs_path_files():
+    pass
 
 def get_file_metadata():
-    # optional
-    pass
+
+
 
 
 def pick_random_set_of_songs():
@@ -54,7 +68,12 @@ def print_list():
 
 
 if __name__ == "__main__":
-    import os, glob
+    import os, glob, csv
 
+    init_cwd = os.getcwd()
+    song_list,root_dir = find_files()
+    print("Adding to list...")
+    os.chdir(init_cwd)
+    print(root_dir)
+    create_csv(song_list)
 
-    find_files()
